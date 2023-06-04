2ff447dbac594ff6bf64848cfc97e5cc75818c7a
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-06-04 11:56:58 +0700

    dev
    Former-commit-id: f1c77229fef34664277641507dd7b4c0c92b7742 [formerly bcd96d3b30ea0d4bc35403e8f601cdc6259896b6]
    Former-commit-id: 69fef7c375259019bf1017cc4f7078dace0640af

--- package/src/build/naive_bayes/mod.rs
+++ package/src/build/naive_bayes/mod.rs
@@ -2,6 +2,7 @@ use linfa_preprocessing::CountVectorizer;
 use linfa_bayes::{GaussianNbParams, GaussianNbValidParams, Result};
 use linfa::prelude::*;
 use ndarray::{ArrayBase, Dim, OwnedRepr};
+use ndarray::Axis;
 
 pub fn update_naive_bayes_model(x_dataset: Vec<String>, y_dataset: Vec<i32>,test_x_dataset: Vec<String>, test_y_dataset: Vec<i32>) ->  anyhow::Result<()> {
 
@@ -33,6 +34,16 @@ pub fn update_naive_bayes_model(x_dataset: Vec<String>, y_dataset: Vec<i32>,test
         training_records.dim().1
     );
 
+    // Displaying sample entries of the training data
+    println!("Sample entries of the training data:");
+    let num_samples = 5;
+    for i in 0..num_samples {
+        let sample_entry = training_records.index_axis(Axis(0), i);
+        let label = labels[i];
+        let text = texts[i].to_owned();
+        println!("Text: {}, Data: {:?}, Label: {}", text, sample_entry, label);
+    }
+
     let ds = DatasetView::new(training_records.view(), labels.view());
 
     // Transforming gives a sparse dataset, we make it dense in order to be able to fit the Naive Bayes model
@@ -45,6 +56,16 @@ pub fn update_naive_bayes_model(x_dataset: Vec<String>, y_dataset: Vec<i32>,test
         test_records.dim().0,
         test_records.dim().1
     );
+
+
+    // Displaying sample entries of the test data
+    println!("Sample entries of the test data:");
+    for i in 0..num_samples {
+        let sample_entry = test_records.index_axis(Axis(0), i);
+        let label = test_labels[i];
+        let text = test_texts[i].to_owned();
+        println!("Text: {}, Data: {:?}, Label: {}", text, sample_entry, label);
+    }
 
     let test_ds = DatasetView::new(test_records.view(), test_labels.view());
 
@@ -64,6 +85,23 @@ pub fn update_naive_bayes_model(x_dataset: Vec<String>, y_dataset: Vec<i32>,test
     let model = checked_params.fit_with(Some(model), &ds).unwrap().unwrap();
 
     let training_prediction = model.predict(&test_ds);
+
+
+    // Displaying predictions
+    println!("Predictions:");
+    println!("Predictions:");
+    let num_predictions = 5;
+    let prediction_labels = training_prediction
+        .labels()
+        .iter()
+        .take(num_predictions)
+        .collect::<Vec<_>>();
+    for i in 0..num_predictions {
+        let prediction = prediction_labels[i];
+        let true_label = test_labels[i];
+        let text = test_texts[i].to_owned();
+        println!("Text: {}, Prediction: {}, True Label: {}", text, prediction, true_label);
+    }
 
     let cm = training_prediction
         .confusion_matrix(&test_ds)
