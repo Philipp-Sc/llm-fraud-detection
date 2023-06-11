a2c5d78da03240f37bf4247987db1a47554eec01
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-06-11 12:27:10 +0700

    dev
    Former-commit-id: cbc8ba21e0539ce7b9de6dfa434b88181d956c1f [formerly add116c6ad1e94d680ebdf5b741e2157385e8046]
    Former-commit-id: e711ed453347f088d394cbf2508f680d4996cdb5

--- package/src/build/naive_bayes/mod.rs
+++ package/src/build/naive_bayes/mod.rs
@@ -285,7 +285,7 @@ pub fn update_categorical_naive_bayes_model(x_dataset: Vec<String>, y_dataset: V
     );
     println!();
 
-    let x_data = training_records.outer_iter().map(|row| row.to_vec().into_iter().map(|x| x as f32).collect::<Vec<f32>>().as_slice()).collect::<Vec<&[f32]>>();
+    let x_data = training_records.outer_iter().map(|row| row.to_vec().into_iter().map(|x| x as f32).collect::<Vec<f32>>()).map(|x| x.as_slice()).collect::<Vec<&[f32]>>();
 
     let x = DenseMatrix::<f32>::from_2d_array(x_data.as_slice());
 
