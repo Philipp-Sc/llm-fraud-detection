91bfaf963af1d73a53a3d1f5db30ea423027f0a3
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-07-05 15:33:38 +0700

    update
    Former-commit-id: e1407f3aee68ea575c2576b3b5a102555f5351b8 [formerly 91919e5682a89f168343effebb5540e6dff91b7d]
    Former-commit-id: 3daa4c0f22c62a9ae8cccf7424a3552d34f9ffaf

--- package/src/build/feature_engineering/mod.rs
+++ package/src/build/feature_engineering/mod.rs
@@ -4,9 +4,9 @@ lazy_static::lazy_static! {
 
         static ref RE_URL: regex::Regex = regex::Regex::new(r"(http|https|www)").unwrap();
 
-        static ref RE_NUMBER: regex::Regex = regex::Regex::new(r"\d+").unwrap();
+ //       static ref RE_NUMBER: regex::Regex = regex::Regex::new(r"\d+").unwrap();
 
-        static ref RE_UPPER_CASE_WORD: regex::Regex = regex::Regex::new(r"\b[A-Z]+\b").unwrap();
+ //       static ref RE_UPPER_CASE_WORD: regex::Regex = regex::Regex::new(r"\b[A-Z]+\b").unwrap();
         
         static ref RE_NON_STANDARD: regex::Regex = regex::Regex::new(r"[^\w\s]").unwrap();
         
@@ -36,21 +36,21 @@ pub fn get_features(text: String) -> Vec<f64> {
     let mut features = Vec::new();
     
     // Naive Bayes Spam Prediction
-    features.push(super::naive_bayes::categorical_nb_model_predict(vec![text.clone()]).unwrap()[0] as f64);
-    features.push(super::naive_bayes::gaussian_nb_model_predict(vec![text.clone()]).unwrap()[0] as f64);
+    features.push(super::naive_bayes::categorical_nb_model_predict(vec![text.clone()]).unwrap()[0] as f64); // 2
+    features.push(super::naive_bayes::gaussian_nb_model_predict(vec![text.clone()]).unwrap()[0] as f64); // 1
 
 
     let word_count = words_count::count(&text);
-    features.push(word_count.words as f64);
-    features.push(word_count.characters as f64);
-    features.push(word_count.whitespaces as f64);
-    features.push(word_count.cjk as f64);
+    features.push(word_count.words as f64); // 8
+    features.push(word_count.characters as f64); // 3
+    features.push(word_count.whitespaces as f64); // 5
+    //features.push(word_count.cjk as f64);
 
-    features.push(RE_URL.captures_iter(&text.to_lowercase()).count() as f64);
-    features.push(RE_UPPER_CASE_WORD.captures_iter(&text).count() as f64);
-    features.push(RE_NON_STANDARD.captures_iter(&text).count() as f64);
-    features.push(RE_PUNCTUATION.captures_iter(&text).count() as f64);
-    features.push(RE_EMOJI.captures_iter(&text).count() as f64);
+    features.push(RE_URL.captures_iter(&text.to_lowercase()).count() as f64); // 9
+    //features.push(RE_UPPER_CASE_WORD.captures_iter(&text).count() as f64);
+    features.push(RE_NON_STANDARD.captures_iter(&text).count() as f64); // 7
+    features.push(RE_PUNCTUATION.captures_iter(&text).count() as f64); // 4
+    features.push(RE_EMOJI.captures_iter(&text).count() as f64); // 6
 
     features
 }
