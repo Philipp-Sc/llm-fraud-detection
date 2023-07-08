5a2f258dbd300816746c6425f87724c4fb5060cf
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-07-08 17:42:18 +0700

    update
    Former-commit-id: 8612802d04d2d1e17753cb7f97996896f929df02 [formerly eb0b7a11dd9ac8fd20265872a5655964892bf46d]
    Former-commit-id: aba3fbc929511cfdec697ae03cb232cc698c9a17

--- package/src/build/classification/deep_learning/mod.rs
+++ package/src/build/classification/deep_learning/mod.rs
@@ -9,24 +9,45 @@ pub struct Predictor {
     linear2: nn::Linear,
     linear3: nn::Linear,
     linear4: nn::Linear,
+    linear5: nn::Linear,
+    batch_norm1: nn::BatchNorm,
+    batch_norm2: nn::BatchNorm,
+    batch_norm3: nn::BatchNorm,
+    batch_norm4: nn::BatchNorm,
+    dropout_p: f64,
 }
 
 impl Predictor {
     fn new(vs: &nn::Path, input_size: i64) -> Self {
-        let hidden_size1 = input_size * 8;
+        let hidden_size1 = input_size * 4;
         let hidden_size2 = hidden_size1 * 2;
         let hidden_size3 = hidden_size1;
+        let hidden_size4 = hidden_size1 * 2;
 
         let linear1 = nn::linear(vs, input_size, hidden_size1, Default::default());
         let linear2 = nn::linear(vs, hidden_size1, hidden_size2, Default::default());
         let linear3 = nn::linear(vs, hidden_size2, hidden_size3, Default::default());
-        let linear4 = nn::linear(vs, hidden_size3, 1, Default::default());
+        let linear4 = nn::linear(vs, hidden_size3, hidden_size4, Default::default());
+        let linear5 = nn::linear(vs, hidden_size4, 1, Default::default());
+
+        let batch_norm1 = nn::batch_norm1d(vs, hidden_size1, Default::default());
+        let batch_norm2 = nn::batch_norm1d(vs, hidden_size2, Default::default());
+        let batch_norm3 = nn::batch_norm1d(vs, hidden_size3, Default::default());
+        let batch_norm4 = nn::batch_norm1d(vs, hidden_size4, Default::default());
+
+        let dropout_p = 0.5;
 
         Predictor {
             linear1,
             linear2,
             linear3,
             linear4,
+            linear5,
+            batch_norm1,
+            batch_norm2,
+            batch_norm3,
+            batch_norm4,
+            dropout_p,
         }
     }
 }
@@ -35,12 +56,23 @@ impl Module for Predictor {
     fn forward(&self, input: &Tensor) -> Tensor {
         let output = input
             .apply(&self.linear1)
+            .apply_t(&self.batch_norm1, true)
             .relu()
+            .dropout(self.dropout_p, true)
             .apply(&self.linear2)
+            .apply_t(&self.batch_norm2, true)
             .relu()
+            .dropout(self.dropout_p, true)
             .apply(&self.linear3)
+            .apply_t(&self.batch_norm3, true)
             .relu()
-            .apply(&self.linear4);
+            .dropout(self.dropout_p, true)
+            .apply(&self.linear4)
+            .apply_t(&self.batch_norm4, true)
+            .relu()
+            .dropout(self.dropout_p, true)
+            .apply(&self.linear5)
+            .sigmoid();
         output
     }
 }
@@ -63,9 +95,11 @@ pub fn train_nn(x_dataset: &Vec<Vec<f64>>, y_dataset: &Vec<f64>) -> Predictor {
 
     let mut optimizer = nn::Adam::default().build(&vs, 1e-3).unwrap();
 
-    for epoch in 0..1000 {
+    for epoch in 0..500 {
         let output = predictor.forward(&input_tensor);
-        let loss = output.mse_loss(&target_tensor, tch::Reduction::Mean);
+        //let loss = output.mse_loss(&target_tensor, tch::Reduction::Mean);
+        let loss = output.binary_cross_entropy(&output, Some(&target_tensor), tch::Reduction::Mean);
+        let loss = tch::nn::functional::binary_cross_entropy_with_logits(&output, &target_tensor, None, tch::Reduction::Mean);
         optimizer.zero_grad();
         loss.backward();
         optimizer.step();
