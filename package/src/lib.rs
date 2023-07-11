17427f1ad2c93c3825224850a83241d80302f240
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-07-11 16:32:42 +0700

    dev
    Former-commit-id: 6a8de6f176a31540804437d005768b93ac3a7413 [formerly b1aceb8c09a30a7a0a95e3515461d2052549efbf]
    Former-commit-id: a6e600da20bd5403e9dd751cf0e44accdb1385cd

--- package/src/lib.rs
+++ package/src/lib.rs
@@ -3,20 +3,33 @@ pub mod cache;
 
 pub mod service;
 
+use importance::score::Model;
+use crate::build::classification::deep_learning::MockModel;
 use crate::build::feature_engineering::get_features;
 
 pub fn fraud_probabilities(texts: &[&str]/*, topics: &[&str]*/) ->  anyhow::Result<Vec<f64>> {
-    // gets topic predictions
-    let mut topic_predictions: Vec<Vec<f64>> = build::language_model::get_topic_predictions(texts,&build::FRAUD_INDICATORS)?;
 
+
+
+    let mut topic_predictions: Vec<Vec<f64>> = build::language_model::get_topic_predictions(texts,&build::FRAUD_INDICATORS)?;
     let sentiment_predictions = build::sentiment::get_sentiments(texts);
-    // add custom features
-    // add sentiment predictions
+
+
+    let mut input: Vec<Vec<f64>> = Vec::with_capacity(texts.len());
     for i in 0..texts.len() {
         let text = texts[i].to_owned();
-        topic_predictions[i].append(&mut get_features(&text));
-        topic_predictions[i].push(sentiment_predictions[i]);
+        input[i].append(&mut get_features(&text));
+
+        let model = MockModel{ label: "./NeuralNet.bin".to_string()};
+        let prediction: f64 = model.predict(&vec![topic_predictions[i].clone()])[0];
+
+        input[i].append(&mut topic_predictions[i]);
+        input[i].push(prediction);
+
+        input[i].push(sentiment_predictions[i]);
+
     }
+
 
 
     build::classification::predict(&topic_predictions)
